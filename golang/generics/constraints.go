fac9f485031c515254db15eea96fd18fe2332fa2
Oti254 <otrated555@gmail.com>
2026-01-22 01:06:33 +0300

    using constraints with generics

---
+++ golang/generics/constraints.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+func chargeForLineItem[T lineItem](newItem T, oldItems []T, balance float64) ([]T, float64, error) {
+	//setting the zero values
+	var myZeroT []T
+	var myZerofloat float64
+
+	// Checking if user has enough funds
+	if balance >= lineItem.GetCost(newItem) {
+		oldItems = append(oldItems, newItem)
+		newBalance := balance - lineItem.GetCost(newItem)
+		return oldItems, newBalance, nil
+	}
+	
+	// If balance is insufficient return zero values
+	return myZeroT, myZerofloat, errors.New("insufficient funds")
+}
+
+// don't edit below this line
+
+type lineItem interface {
+	GetCost() float64
+	GetName() string
+}
+
+type subscription struct {
+	userEmail string
+	startDate time.Time
+	interval  string
+}
+
+func (s subscription) GetName() string {
+	return fmt.Sprintf("%s subscription", s.interval)
+}
+
+func (s subscription) GetCost() float64 {
+	if s.interval == "monthly" {
+		return 25.00
+	}
+	if s.interval == "yearly" {
+		return 250.00
+	}
+	return 0.0
+}
+
+type oneTimeUsagePlan struct {
+	userEmail        string
+	numEmailsAllowed int
+}
+
+func (otup oneTimeUsagePlan) GetName() string {
+	return fmt.Sprintf("one time usage plan with %v emails", otup.numEmailsAllowed)
+}
+
+func (otup oneTimeUsagePlan) GetCost() float64 {
+	const costPerEmail = 0.03
+	return float64(otup.numEmailsAllowed) * costPerEmail
+}
+
